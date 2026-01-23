5ada22c4cbc1291209117bff67c13251b445a672
ur.gergely <ur.gergely@gmail.com>
2026-01-23 13:52:10 +0100

    Ex_5

--- Ex_5/Ex_5.cs
+++ Ex_5/Ex_5.cs
@@ -7,13 +7,14 @@
             /*Készítsünk programot, amely elkéri a felhasználó testmagasságát(h, méterben) és testtömegét(m, kilo-
             grammban), majd kiszámítja és kiírja a felhasználó testtömegindexét(BM I).A számításhoz használt formula:BMI = m/(h*h)*/
 
-            Console.Write("Kérem adja meg a testmagasságát méterben: ");
-            double h = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Kérem adja meg a testtömegét kilogrammban: ");
-            double m = Convert.ToDouble(Console.ReadLine());
-            double BMI = m / (h * h);
-            Console.WriteLine($"Az Ön testtömegindexe: {BMI:F2}");
-            Console.ReadKey();
+            Console.Write("Enter your height in meters: ");
+            double height = double.Parse(Console.ReadLine());
+            Console.Write("Enter your weight in kilograms: ");
+            double weight = double.Parse(Console.ReadLine());
+
+            double BMI = weight / (height * height);
+            Console.WriteLine($"Your body mass index is {BMI:F2}.");
+            Console.ReadLine();
         }
     }
 }
