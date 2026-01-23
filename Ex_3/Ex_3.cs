c0554cb0a604fa344ca9080d1d9cb0831d67cca0
ur.gergely <ur.gergely@gmail.com>
2026-01-23 12:43:05 +0100

    Ex_3

---
+++ Ex_3/Ex_3.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Készítsünk programot, amely elkéri a felhasználó nevét, majd név szerint köszönti őt.
+
+            Console.Write("Add meg a neved: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+            Console.ReadLine();
+        }
+    }
+}
