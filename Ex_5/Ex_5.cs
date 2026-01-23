6d98b8c71e47b4d6e0315875c0a57adf27c3ae13
ur.gergely <ur.gergely@gmail.com>
2026-01-23 13:09:09 +0100

    Ex_5

---
+++ Ex_5/Ex_5.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Készítsünk programot, amely elkéri a felhasználó testmagasságát(h, méterben) és testtömegét(m, kilo-
+            grammban), majd kiszámítja és kiírja a felhasználó testtömegindexét(BM I).A számításhoz használt formula:BMI = m/(h*h)*/
+
+            Console.Write("Kérem adja meg a testmagasságát méterben: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Kérem adja meg a testtömegét kilogrammban: ");
+            double m = Convert.ToDouble(Console.ReadLine());
+            double BMI = m / (h * h);
+            Console.WriteLine($"Az Ön testtömegindexe: {BMI:F2}");
+            Console.ReadKey();
+        }
+    }
+}
