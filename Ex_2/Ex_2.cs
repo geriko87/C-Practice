a32e3bb361f1e398a8c05a6ee70a47de33b96e0f
ur.gergely <ur.gergely@gmail.com>
2026-01-23 12:37:01 +0100

    Ex_2

---
+++ Ex_2/Ex_2.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Bővítsük ki az előbbi programot: helyezzünk el néhányat az alábbi utasításokból a szöveg megjelenítését
+            végző utasítás elé vagy után. Az utasítások a kurzor és a parancssori ablak jellemzőit módosítják.
+            • Console.Clear()
+            • Console.WindowHeight
+            • Console.WindowWidth
+            • Console.BackgroundColor
+            • Console.ForegroundColor
+            • Console.SetCursorPosition()
+            • Console.CursorVisible*/
+
+            Console.WriteLine("Hello, World!");
+            Console.WindowHeight = 20;
+            Console.WindowWidth = 120;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(10, 5);
+            Console.CursorVisible = false;
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
