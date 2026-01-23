4bd4a3ea0d2e64ec60a37238bda912a7a80507c5
ur.gergely <ur.gergely@gmail.com>
2026-01-23 13:42:57 +0100

    Ex_4

--- Ex_4/Ex_4.cs
+++ Ex_4/Ex_4.cs
@@ -7,12 +7,12 @@
             /*Készítsünk programot, amely elkéri a felhasználó születési évét, ez alapján pedig kiszámítja és kiírja az
             életkorát.A program írja ki azt is, hány éves lesz a felhasználó a következő évben.*/
 
-            Console.Write("Kérem adja meg a születési évét: ");
-            int szuletesiEv = Convert.ToInt32(Console.ReadLine());
-            int jelenlegiEv = DateTime.Now.Year;
-            int eletkor = jelenlegiEv - szuletesiEv;
-            Console.WriteLine($"Az Ön életkora: {eletkor} év.");
-            Console.WriteLine($"A következő évben {eletkor + 1} éves lesz.");
+            Console.Write("Enter your year of birth: ");
+            int yearOfBirth = int.Parse(Console.ReadLine());
+            int currentYear = DateTime.Now.Year;
+            int age = currentYear - yearOfBirth;
+            Console.WriteLine($"You are now {age} years old.");
+            Console.WriteLine($"You will be {age + 1} next year.");
             Console.ReadLine();
         }
     }
