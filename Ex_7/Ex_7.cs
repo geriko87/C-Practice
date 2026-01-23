b3cefe73045b530eb4767e1165822e8650e853a2
ur.gergely <ur.gergely@gmail.com>
2026-01-23 14:11:01 +0100

    Ex_7

---
+++ Ex_7/Ex_7.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Kérjük el a felhasználótól a jelszavát, majd kérjük el még egyszer megerősítésképp.Ha egyezik a két
+            megadott jelszó, nyugtázzuk egy zöld színnel kiírt üzenettel, ellenkező esetben jelenítsünk meg egy piros színű
+            hibaüzenetet.*/
+
+            Console.Write("Enter password: ");
+            string password1 = Console.ReadLine();
+            Console.Write("Enter the password again: ");
+            string password2 = Console.ReadLine();
+
+            if (password1 != password2)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Incorrect password!");
+            }
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Correct password.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
