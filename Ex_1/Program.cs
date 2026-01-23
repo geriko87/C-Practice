2ba663a577401fe1f7cec77508b924bef5b43edc
ur.gergely <ur.gergely@gmail.com>
2026-01-23 12:19:28 +0100

    Ex_1

---
+++ Ex_1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
