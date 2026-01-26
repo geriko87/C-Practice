e1505a1753c152dbc778a6b7214159156042c9a7
ur.gergely <ur.gergely@gmail.com>
2026-01-26 23:47:02 +0100

    C# Practice

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kérjünk el egy pozitív egész számot, majd írjuk ki a faktoriálisát.
+            //N! = N × (N − 1) × (N − 2) × . . . × 3 × 2 × 1
+            //Példa
+            //N = 5
+            //5! = 5×4×3×2×1 = 120
+
+            Console.Write("Enter a number and I'll write out its factorial: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int factorial = number;
+
+            Console.Write($"{number}! = {number}");
+            for (int i = number - 1; i > 0; --i) 
+            {
+                factorial *= i;
+                Console.Write("*" + i);
+            }
+            Console.WriteLine(" = " + factorial);
+            Console.ReadLine();
+        }
+    }
+}
