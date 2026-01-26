c1322c4024deae9e68c10100335b7ee0a3e2074a
ur.gergely <ur.gergely@gmail.com>
2026-01-26 22:56:50 +0100

    C# Practice

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -7,15 +7,16 @@
             //Írjunk programot, amely addig generál véletlen számokat 1 és 1000 között, amíg az meg nem egyezik a
             //program kezdetén a felhasználó által megadott számmal.Számoljuk meg, hány próbálkozás kellett a találathoz.
 
+            Random rnd = new Random();
+
             Console.Write("Enter a positive integer between 1 and 1000: ");
             int number = int.Parse(Console.ReadLine());
             int rndNumber;
             int attempts = 0;
 
-            Random rnd = new Random();
             do
             {
-                rndNumber = rnd.Next(1001);
+                rndNumber = rnd.Next(1, 1001);
                 attempts++;
 
                 Console.WriteLine(rndNumber);
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Írjunk programot, amelynek kezdetén adott egy pozitív egész szám, a „gondolt szám”. A felhasználónak ki
+            //kell találnia, hogy mi a gondolt szám.Ehhez a felhasználó megadhat számokat, melyekről a program megmondja,
+            //hogy a gondolt számnál nagyobbak vagy kisebbek-e.A program akkor ér véget, ha a felhasználó kitalálta a gondolt
+            //számot.A program jelenítse meg a felhasználó próbálkozásainak számát is.
+
+            int thoughtNumber = 15;
+            int attempts = 0;
+
+            bool result = false;
+            while (!result) 
+            {
+                Console.Write("Enter a number and I'll check if your guess was successful: ");
+                int bet = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                attempts++;
+                if (bet == thoughtNumber)
+                {
+                    Console.WriteLine($"Congratulations! It took {attempts} attempts.\nPress enter to exit!");
+                    Console.ReadLine();
+                    result = true;
+                    break;
+                }
+                else 
+                {
+                    if (bet < thoughtNumber)
+                    {
+                        Console.WriteLine("Wrong guess! The entered number is smaller.\nTry again! Press a button to continue!");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Wrong guess! The entered number is bigger.\nTry again! Press a button to continue!");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+            }
+
+        }
+    }
+}
