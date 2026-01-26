dc94c5367b012c9655d908a2af372b6f6ef19782
ur.gergely <ur.gergely@gmail.com>
2026-01-26 21:12:59 +0100

    C# Practice

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Hozzunk létre egy új parancssoros projektet, majd írjunk programot, amely megjeleníti a Hello, World!
+            szöveget a képernyőn.Az alkalmazás az Enter billentyű leütésekor érjen véget.*/
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
