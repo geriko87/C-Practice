c9b82f06ebd2c4ebcf0324ac38c1f198e7ebdba8
ur.gergely <ur.gergely@gmail.com>
2026-01-26 21:32:45 +0100

    C# Practice

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Írjunk programot, amely addig generál véletlen számokat 1 és 1000 között, amíg az meg nem egyezik a
+            //program kezdetén a felhasználó által megadott számmal.Számoljuk meg, hány próbálkozás kellett a találathoz.
+
+            Console.Write("Enter a positive integer between 1 and 1000: ");
+            int number = int.Parse(Console.ReadLine());
+            int rndNumber;
+            int attempts = 0;
+
+            Random rnd = new Random();
+            do
+            {
+                rndNumber = rnd.Next(1001);
+                attempts++;
+
+                Console.WriteLine(rndNumber);
+            } while (rndNumber != number);
+
+            Console.WriteLine($"It took {attempts} attempts.");
+            Console.ReadLine();
+        }
+    }
+}
