53d1c213793e8832e082a42052948549485cd1fd
ur.gergely <ur.gergely@gmail.com>
2026-01-27 21:12:52 +0100

    C# Practice

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Készítsük programot, amely kiírja a képernyőre a szorzótáblát az alábbihoz hasonlóan.
+
+            for (int i = 1; i < 10; ++i) 
+            {
+                for (int j = 1; j < 10; ++j) 
+                {
+                    Console.Write($"{i * j, 2}");
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
