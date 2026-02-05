190cd72737b427678c706707eee63afe244fbd2b
ur.gergely <ur.gergely@gmail.com>
2026-02-05 20:38:44 +0100

    C# Practice

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kérjünk el a felhasználótól előre megadott darabszámú szót, amelyeket tároljunk el egy tömbben. Ezután
+            //kérjünk el a felhasználótól egy további szót, és válaszoljuk meg az alábbiakat.
+            //• Benne van-e a gyűjteményben a megadott szó ?
+            //• Ha benne van, hol található először ?
+
+            string[] words = new string[5];
+
+            for (int i = 0; i < words.Length; ++i)
+            {
+                Console.Write($"Add meg az {i + 1}. szót: ");
+                string input = Console.ReadLine();
+                Console.Clear();
+                words[i] = input;
+            }
+
+            Console.Write($"Add meg a keresett szót: ");
+            string searchedWord = Console.ReadLine();
+            Console.Clear();
+
+            bool result = false;
+            int index = 0;
+            foreach (string w in words) 
+            {
+                index++;
+                if (w == searchedWord)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            if (result)
+            {
+                Console.WriteLine($"A keresett szó megtalálható a {index}. helyen.");
+            }
+            else 
+            {
+                Console.WriteLine("A keresett szó nem található.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
